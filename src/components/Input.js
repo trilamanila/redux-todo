adfa9b65f093d222869f1e0a62ed7c10d7a9a7dd
JC <jctrilamanila@gmail.com>
2021-03-14 23:27:33 -0400

    setup todo slice

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
 import "./Input.css";
 
+import { useDispatch } from "react-redux";
+import saveTodo from "../features/todoSlice";
+
 const Input = () => {
   const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
-  const addTodo = () => {};
+  const addTodo = () => {
+    console.log(`Adding ${input}`);
+
+    dispatch(
+      saveTodo({
+        item: input,
+        done: false,
+        id: Date.now()
+      })
+    );
+  };
   return (
     <div className="input">
-      <input type="text" value={input} onChange={e => setInput} />
+      <input
+        type="text"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
       <button onClick={addTodo}>Add</button>
     </div>
   );
